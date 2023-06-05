bef689ba3226b6e415ac24064d4b8257d22f0035
MARIE Clément <clement.marie24@gmail.com>
2023-06-05 10:02:27 +0200

    test

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module sae201.sae {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires com.dlsc.formsfx;
+    requires eu.hansolo.tilesfx;
+
+    opens sae201.sae to javafx.fxml;
+    exports sae201.sae;
+}
