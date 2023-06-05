1fef3098780f78e2a5f74e8e2d60ffbb481d7d3b
b22006496 <chiheb.bradai@etu.univ-amu.fr>
2023-06-05 11:26:11 +0200

    le commit

--- src/main/java/sae201/sae/HelloController.java
+++ src/main/java/sae201/sae/HelloController.java
@@ -1,51 +1,43 @@
 package sae201.sae;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+
 import java.util.ArrayList;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    private Button btn;
+    private BorderPane bdp;
+    @FXML
+    public void lireDonees(){
+        String csvFile = "src/main/resources/sae201/sae/donnee.csv";
+        String line;
+        String cvsSplitBy = ",";
 
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(cvsSplitBy);
+                String colonne1 = values[0];
+                String colonne2 = values[1];
+                System.out.println(line);
+                Label donneeLabel = new Label(line);
+                bdp.getChildren().add(donneeLabel);
 
-/*
-    public void ouvrirFichierCSV() {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle("Ouvrir un fichier CSV");
-        fileChooser.getExtensionFilters().add(
-                new FileChooser.ExtensionFilter("../../ressources/sae201.sae/donne.csv", "*.csv"));
-
-        File fichierCSV = fileChooser.showOpenDialog(new Stage());
-
-        if (fichierCSV != null) {
-            lireFichierCSV(fichierCSV);
-        }
-
-    }public void lireFichierCSV(File fichierCSV) {
-        try (BufferedReader br = new BufferedReader(new FileReader(fichierCSV))) {
-            String ligne;
-            while ((ligne = br.readLine()) != null) {
-                String[] donnees = ligne.split(","); // Modifier le séparateur si nécessaire
-                // Traitez les données du fichier CSV ici
-                // Exemple : Affichage des données dans la console
-                for (String donnee : donnees) {
-                    System.out.print(donnee + " ");
-                }
-                System.out.println();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
     }
-
-}
-*/
-
 }
