5f1735ae675bf05c933ffe7e632b8de6796dae97
b22006496 <chiheb.bradai@etu.univ-amu.fr>
2023-06-05 11:55:16 +0200

    le commit

--- src/main/java/sae201/sae/HelloController.java
+++ src/main/java/sae201/sae/HelloController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -17,27 +18,31 @@ import java.util.ArrayList;
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
     private Button btn;
+    @FXML
     private BorderPane bdp;
     @FXML
     public void lireDonees(){
-        String csvFile = "src/main/resources/sae201/sae/donnee.csv";
+        String csvFile = "src/main/resources/sae201/sae/donne.csv";
         String line;
         String cvsSplitBy = ",";
-
+        VBox vbox = new VBox();
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(cvsSplitBy);
-                String colonne1 = values[0];
-                String colonne2 = values[1];
                 System.out.println(line);
-                Label donneeLabel = new Label(line);
-                bdp.getChildren().add(donneeLabel);
-
+                for (int i = 0; i < values.length; i++) {
+                    values[i] = values[i].replaceAll("\"", "");
+                }
+                for (int i = 0; i<values.length;i++){
+                    Label donneeLabel = new Label(values[i]);
+                    vbox.getChildren().add(donneeLabel);
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+        bdp.setCenter(vbox);
     }
 }
